6cfb627fc3452e6a5fc5e46221494f5cd51e8722
AlexoMetaute <120112728+AlexoMetaute@users.noreply.github.com>
2023-06-11 20:27:25 -0500

    CHALLENGE ALURAGEEK

---
+++ assets/js/slider.js
@@ -0,0 +1,17 @@
+/*first presentation*/
+let count = 1;
+/*Every 7 seconds*/
+setInterval(slider, 7000);
+
+function slider() {
+    let presentation = document.querySelectorAll(".section__banner");
+    if (count > (presentation.length - 1)) {
+        count = 0;
+    }
+
+    for (var x = 0; x < presentation.length; x++) {
+        presentation[x].style.display = "none";
+    }
+    presentation[count].style.display = "block";
+    count++;
+}
